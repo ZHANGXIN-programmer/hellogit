8dcf86a37a343922f5cca17d0fc0e5628d7e5f06
zhangxin <zhangxin@qq.com>
2021-01-21 04:30:14 +0900

    创建分支

--- HelloGit/src/main/java/hanyang/zhangxin/HelloGit.java
+++ HelloGit/src/main/java/hanyang/zhangxin/HelloGit.java
@@ -3,5 +3,6 @@ package hanyang.zhangxin;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("创建分支");
     }
 }
